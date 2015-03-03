ac4225ae56796ccd832942e6738603c38be6ac4e
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-03-03 17:07:21 -0300

    Refatoracao do projeto
    Agora o projeto esta rodando com o Ionic e o Cordova. Todo os arquivos do projeto antigo foram movidos para a pasta old.

--- sincap-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/marcosdias/IdeaProjects/sincap-mobile/sincap-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/marcosdias/IdeaProjects/sincap-mobile/sincap-mobile/platforms/android/CordovaLib/AndroidManifest.xml \
--- sincap-mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/marcosdias/IdeaProjects/sincap-mobile/sincap-mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/marcosdias/IdeaProjects/sincap-mobile/sincap-mobile/platforms/android/ant-build/CordovaApp.ap_ \
-/home/marcosdias/IdeaProjects/sincap-mobile/sincap-mobile/platforms/android/ant-build/classes.dex \
-/home/marcosdias/IdeaProjects/sincap-mobile/sincap-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
